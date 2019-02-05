9dfae51b6d1324fe6cf448b73c9f1f3515b4cf73
Konsta Purtsi <kovipu@utu.fi>
2019-02-05 18:23:52 +0200

    Added validating for new requests

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
 
 const app = express();
@@ -8,10 +8,40 @@ app.use(bodyParser.json());
  * POST /api/newreading
  * Send a new reading from the sensor to the server
  */
-app.post('/api/newreading', (req, res) => {
-  console.log('received new reading:', req.body);
-  res.send(req.body);
+app.post('/api/newreading', (req: Request, res: Response) => {
+  const reading: NewReading = req.body;
+  console.log('received new reading:', reading);
+
+  try {
+    assertReading(reading);
+  }
+  catch (error) {
+    return res.status(400).send(error); // HTTP 400 Bad Request
+  }
+
+  res.send(reading);
 });
+
+const assertReading = (reading: NewReading | null) => {
+  if (!reading) {
+    throw 'Invalid request body';
+  }
+
+  const { name, temperature, pressure, humidity } = reading;
+
+  if (!name || typeof name !== 'string' || name.length < 3) {
+    throw 'Invalid or missing parameter "name"';
+  }
+  else if (!temperature || typeof temperature !== 'number') {
+    throw 'Invalid or missing parameter "temperature"';
+  }
+  else if (!pressure || typeof pressure !== 'number') {
+    throw 'Invalid or missing parameter "pressure"';
+  }
+  else if (!humidity || typeof humidity !== 'number') {
+    throw 'Invalid or missing parameter "humidity"';
+  }
+};
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
