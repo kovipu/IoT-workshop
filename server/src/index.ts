19725d397f4d31cb26c3a4b5db9bc37b74c979e3
Konsta Purtsi <kovipu@utu.fi>
2019-02-05 18:11:38 +0200

    Added endpoint for posting a new request

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,19 @@
-console.log("Helou koodi- ja eltykerho!");
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const app = express();
+app.use(bodyParser.json());
+
+/**
+ * POST /api/newreading
+ * Send a new reading from the sensor to the server
+ */
+app.post('/api/newreading', (req, res) => {
+  console.log('received new reading:', req.body);
+  res.send(req.body);
+});
+
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+  console.log('Server listening on port ' + port);
+});
