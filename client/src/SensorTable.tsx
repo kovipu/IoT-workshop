bce3180e97778aae5f5977ab07ee83a498178987
Konsta Purtsi <kovipu@utu.fi>
2019-02-06 18:45:44 +0200

    Made a table for sensors

--- client/src/SensorTable.tsx
+++ client/src/SensorTable.tsx
@@ -7,7 +7,37 @@ interface Props {
 }
 
 const SensorTable = ({ data, error }: Props) => (
-  <div>Sensorit</div>
+  <Table>
+    {
+      error
+        ? error
+        : (
+          <>
+            <thead>
+              <tr>
+                <th>Nimi</th>
+                <th>Ensimmäinen viesti</th>
+                <th>Viimeisin viesti</th>
+              </tr>
+            </thead>
+            <tbody>
+            {
+              data.map(({ name, firstonline, lastonline }) => (
+                <tr key={name}>
+                  <td>{name}</td>
+                  <td>{formatDate(firstonline)}</td>
+                  <td>{formatDate(lastonline)}</td>
+                </tr>
+              ))
+            }
+            </tbody>
+          </>
+        )
+    }
+  </Table>
 );
+
+const formatDate = (date: string): string =>
+  new Date(date).toLocaleString('fi-Fi');
 
 export default SensorTable;
