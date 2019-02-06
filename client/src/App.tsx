65a1e9118108e605adc473bacbb12e75113247a6
Konsta Purtsi <kovipu@utu.fi>
2019-02-06 18:03:03 +0200

    Added fetchSensors

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,18 +2,56 @@ import React, { Component } from 'react';
 
 interface State {
   sensors: {
-    isLoading: boolean,
     data: Sensor[],
     error: any
   },
   readings: {
-    isLoading: boolean,
     data: Reading[],
     error: any
   }
 }
 
 class App extends Component<{}, State> {
+  state = {
+    sensors: {
+      data: [],
+      error: null
+    },
+    readings: {
+      data: [],
+      error: null
+    }
+  };
+
+
+
+  async fetchSensors() {
+    try {
+      const response = await fetch('/api/getsensors');
+
+      if (!response.ok) {
+        throw Error(response.statusText)
+      }
+
+      const data: Sensor[] = await response.json();
+
+      this.setState({
+        sensors: {
+          data,
+          error: null
+        }
+      });
+    }
+    catch (error) {
+      this.setState({
+        sensors: {
+          data: [],
+          error: error.toString()
+        }
+      });
+    }
+  }
+
   render() {
     return (
       <div className="App">
